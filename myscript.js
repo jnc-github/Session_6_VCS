a61cf5f7ce90cbaefc91ed0f404ece2af951202f
jnc-github <87894447+jnc-github@users.noreply.github.com>
2021-11-11 11:58:15 +1100

    Add new section
    new <h1>

--- myscript.js
+++ myscript.js
@@ -1,3 +1,7 @@
 function displayMessage() {
     document.getElementById("message-box").innerHTML += "Hello GitHub!<br/>";
 }
+
+function displayMessage2() {
+    document.getElementById("message-box").innerHTML += "Hello Bump!<br/>";
+}
